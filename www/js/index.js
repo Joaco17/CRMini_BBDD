f79628213ccc9ecdf41335d35a8a87e84a6f779d
Joaquín Bahamonde Llorente <joballo.17@gmail.com>
2015-10-28 23:28:23 +0100

    Proyecto BBDD

--- www/js/index.js
+++ www/js/index.js
@@ -19,32 +19,82 @@
 var confDB = {
    //Variable de comprobacion existencia bd
     existe_db:"",
+    db:"",
 
+    //CONSTRUCTOR
     initialize: function(){
         //Declaracion de la variable existe_db
         this.existe_db= window.localStorage.getItem("existe_db");
-        //Log de ruptura
-        console.log("LA BBDD NO EXISTE");
 
+        //Enlazamos con la base de datos
+        this.db= window.openDatabase("localDB", "1.0","Base de datos SQL",2*1024*1024);
+
+        
         if(this.existe_db == null || this.existe_db == false){
-
-            navigator.notification.confirm(
-            'No existe la BBDD; Que desea hacer?',        // message
-            this.onConfirm,                               // callback
-            'BBDD',                                      // title
-            ['Crear','Cancelar']                        // buttonName
-            );
+            //Log de ruptura
+            console.log("LA BBDD NO EXISTE");
+            this.createDB();
+        }else{
+            console.log("LA BBDD YA EXISTE");
         }
     },
 
 
-    onConfirm: function(buttonIndex) {
-        if(buttonIndex==1){
-            console.log("Has pulsado el boton CREAR");
-            window.localStorage.setItem("existe_db", 1);
-        }else{
-            console.log("Has pulsado el boton CANCELAR");
-        }
+    createDB: function(){
+        console.log("CREAMOS LA BBDD");
+        //Metodo transaction
+        this.db.transaction(this.createLocalDB, this.createDBError, this.createDBSuccess);
+    },
+
+    createLocalDB: function(tx){
+        var sql = "CREATE TABLE IF NOT EXISTS localDB ("+
+                  "id           INTEGER         primary key autoincrement,"+
+                  "nombre       VARCHAR(50)     not null,"+
+                  "posicion     VARCHAR(250)    not null,"+
+                  "dorsal       INTEGER         not null,"+
+                  "email        VARCHAR(250)    not null );"
+            ;
+            tx.executeSql(sql);
+            console.log("TABLA CREADA CORRECTAMENTE");
+
+            //Inserción de datos en la tabla creada anteriormente
+            sql = "INSERT INTO localDB(id, nombre, posicion, dorsal, email)"+
+                  "VALUES(1, 'Joaquin Bahamonde', 'Delantero', 17, 'joballo.17@gmail.com')";
+            tx.executeSql(sql);
+
+            sql = "INSERT INTO localDB(id, nombre, posicion, dorsal, email)"+
+                  "VALUES(2, 'Ivan Estruch', 'Extremo', 18, 'estruch95.b@gmail.com')";
+            tx.executeSql(sql);
+
+            sql = "INSERT INTO localDB(id, nombre, posicion, dorsal, email)"+
+                  "VALUES(3, 'Jose Igualada', 'MedioCentro', 8, 'igualada@hotmail.com')";
+            tx.executeSql(sql);
+
+            sql = "INSERT INTO localDB(id, nombre, posicion, dorsal, email)"+
+                  "VALUES(4, 'Alvaro Banyo', 'Portero', 1, 'albafo@hotmail.com')";
+            tx.executeSql(sql);
+
+            sql = "INSERT INTO localDB(id, nombre, posicion, dorsal, email)"+
+                  "VALUES(5, 'Fran Primo', 'Defensa', 8, 'franpri@gmail.com')";
+            tx.executeSql(sql);
+
+            sql = "INSERT INTO localDB(id, nombre, posicion, dorsal, email)"+
+                  "VALUES(6, 'David Alos', 'Defensa', 22, 'alos@gmail.com')";
+            tx.executeSql(sql);
+
+            sql = "INSERT INTO localDB(id, nombre, posicion, dorsal, email)"+
+                  "VALUES(7, 'Nico Pons', 'Defensa', 44, 'nicopons24@gmail.com')";
+            tx.executeSql(sql);
+            console.log("INSERCION DE DATOS REALIZADA CORRECTAMENTE");
+    },
+
+    createDBError: function(err){
+        console.log("ERROR DE CREACION DE BBDD"+error.code);
+    },
+
+    createDBSuccess: function(){
+        console.log("BBDD GENERADA CORRECTAMENTE");
+        window.localStorage.setItem("existe_db",1);
     }
 
 };
@@ -72,14 +122,16 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
        
-       <!-- //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');-->
+        /*var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-        //listeningElement.setAttribute('style', 'display:none;');
-       // receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;'); */
 
         console.log('Received Event: ' + id);
+
+        //SE INICIA LA COMPROBACION DE EXISTENCIA DE db
         confDB.initialize();
     }
 };
